e157f558d456d2a86fb9aecb4ab6b94bfe12cab5
nhinguyen04 <valentinetwin94@gmail.com>
2023-03-21 16:41:51 -0700

    fixed some codes

--- practice-selection-sort/selection-sort.js
+++ practice-selection-sort/selection-sort.js
@@ -27,15 +27,14 @@ function selectionSortInPlace(arr) {
     console.log(arr.join(","));
 
     // find min
-    let min = Infinity;
-    for (let i = div; i < arr.length; i++) {
-      if (arr[i] < min) {
-        min = arr[i];
+    let minIndex = div;
+    for (let i = div + 1; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
       }
     }
 
     // move to div index
-    const minIndex = arr.indexOf(min);
     const spliced = arr.splice(minIndex, 1)[0];
     arr.splice(div, 0, spliced);
     div++;
